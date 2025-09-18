8be1e9d3d9da8d48768f8b5c926d127df732aeaf
Nico-Draagron <nicolas.draagron@gmail.com>
2025-09-18 14:54:47 -0300

    Delete agente_vr directory

--- agente_vr/config/settings.py
+++
@@ -1,4 +0,0 @@
-# Configurações gerais do projeto
-
-# Chave da API do Gemini
-GEMINI_API_KEY = "Coloque a chave da API do Gemini"
--- agente_vr/core/report_generator.py
+++
@@ -1,9 +0,0 @@
-class ReportGenerator:
-    def __init__(self):
-        pass
-    def gerar_planilha_vr(self, *args, **kwargs):
-        return 'planilha_vr.xlsx'
-    def gerar_relatorio_validacoes(self, *args, **kwargs):
-        return 'relatorio_validacoes.txt'
-    def gerar_dashboard(self, *args, **kwargs):
-        return 'dashboard.html'
